e65c4f234c34d43c739a7d2863bd6babd636c262
Joseph-GM <jkim.english75@gmail.com>
2021-12-08 08:15:22 +0900

    Standford Univ. Lecture

--- MemoryGame/Concentration.swift
+++ MemoryGame/Concentration.swift
@@ -6,3 +6,77 @@
 //
 
 import Foundation
+
+class Concentration {
+    private(set) var cards = [Card]()
+    
+//    var indexOfTheOneAndOnlyFaceUpCard: Int?
+    
+    private var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
+    
+    func chooseCard(at index: Int) {
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)): chosen index not in the cards")
+        if !cards[index].isMatched {
+            print("cards[index] = \(cards[index].isMatched)")
+            print("indexOfOneAndOnlyFaceUpCard : \(indexOfTheOneAndOnlyFaceUpCard)")
+            if let matchIndex = indexOfTheOneAndOnlyFaceUpCard, matchIndex != index {
+                //check if cards match
+                print("in matchIndex !!! : \(matchIndex), index : \(index)")
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+//                indexOfTheOneAndOnlyFaceUpCard = nil
+            } else {
+                // either no cards or 2 cards are face up
+                print("in else index = \(index)")
+                // indexOfTheOneAndOnlyFaceUpCard 를 computed property로 바꾸면서 아래 주석을 삭제해도 됨
+//                for flipDownIndex in cards.indices {
+//                    cards[flipDownIndex].isFaceUp = false
+//                }
+//                cards[index].isFaceUp = true
+                indexOfTheOneAndOnlyFaceUpCard = index
+            }
+        }
+//        if cards[index].isFaceUp {
+//            cards[index].isFaceUp = false
+//        } else {
+//            cards[index].isFaceUp = true
+//        }
+    }
+    init(numberOfPairsOfCards: Int) {
+        assert(numberOfPairsOfCards > 0, "Concetration.init(\(numberOfPairsOfCards)): you must have at least one pair of card")
+        for _ in 0..<numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+        // TODO: Shuffle the cards
+        for i in 0..<numberOfPairsOfCards {
+            let randomNumber : Int = Int.random(in: 0..<cards.count)
+            let temp = cards[i]
+            cards[i] = cards[randomNumber]
+            cards[randomNumber] = temp
+        }
+        print("In init of Concentration Class cards.count : \(cards.count)")
+    }
+}
--- MemoryGame/ViewController.swift
+++ MemoryGame/ViewController.swift
@@ -9,11 +9,92 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
+    // 강의에서는 (cardButtons.count+1) / 2라 했음
+    // private으로 하는 것이 좋음 --> 처음에 private을 지정안해도 동작하지만, numberOfPairsOfCards에 종속적이니 ViewController에서만 쓰이는 것이 좋음
+    
+    var numberOfPairsOfCards: Int {
+        return (cardButtons.count+1) / 2
+    }
+    private(set) var flipCount: Int = 0 {  // user가 볼수 있게 하는 것은 괜찮으나 set하면 문제가 있기에 private(set)
+        didSet{
+            flipCountLabel.text = "Flip : \(flipCount)"
+        }
+    }
+    
+    @IBOutlet private var cardButtons: [UIButton]!
+    
+//    var emojiArray : Array<String> = ["👻", "🎃", "👻", "🎃"]
+    
+    @IBOutlet private weak var flipCountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("in viewDidLoad game.cards.count : \(game.cards.count)")
+        print("in viewDidLoad game.indexOfThe... : \(game.indexOfTheOneAndOnlyFaceUpCard)")
         // Do any additional setup after loading the view.
     }
 
+    @IBAction private func touchCard(_ sender: UIButton) {
+        flipCount += 1
+//        for i in 0..<cardButtons.count {
+//            print("game.cards[\(i)] = \(game.cards[i].identifier)")
+//        }
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            print("cardNumber : \(cardNumber)")
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+//            flipCard(withEmoji: emojiArray[cardNumber], on: sender)
+        } else {
+            print("choose proper card")
+        }
+        print("number of CardButton \(cardButtons.count)")
+        
+    }
+    
+    private func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for:.normal)
+                button.backgroundColor = .white
+            } else {
+                button.setTitle("", for: .normal)
+                button.backgroundColor = card.isMatched ? .clear : .orange
+            }
+        }
+    }
+    
+    private var emojiChoices = ["👻", "🎃", "🤡", "☠️","👽","👹","👺","💀","👁"]
+    
+//    var emoji =  Dictionary<Int, String>()
+    private var emoji = [Int:String]() //Same as above statement of Dictionary
+    
+    private func emoji(for card: Card) -> String {
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            //extention 추가후 아래 let 부분 지움
+//            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)) )
+            //extention 추가후 아래 주석이 바뀜
+//            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+        }
+//        if emoji[card.identifier] != nil {
+//            return emoji[card.identifier]!
+//        } else {return "?"}
+        return emoji[card.identifier] ?? "?" //위에 주석이랑 정확히 같은 표현
+    }
+}
 
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(self)))
+        } else {
+            return 0
+        }
+    }
 }
 
